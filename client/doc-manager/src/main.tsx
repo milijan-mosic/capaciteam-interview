bbd0238ba9048b506cb85d951b52825f0573ee12
milijan-mosic <milijan.mosic@gmail.com>
2025-09-04 23:15:37 +0200

    Added React Router

--- client/doc-manager/src/main.tsx
+++ client/doc-manager/src/main.tsx
@@ -1,5 +1,5 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
 
 import "./index.css";
@@ -8,8 +8,10 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root!).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>
+  </BrowserRouter>
 );
