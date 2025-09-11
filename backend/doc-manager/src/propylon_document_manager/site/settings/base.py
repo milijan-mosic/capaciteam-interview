8d833de8c7aac38bc2738d5f258c670d349481bd
milijan-mosic <milijan.mosic@gmail.com>
2025-09-11 14:52:33 +0200

    Connected Django with PostgreSQL

--- backend/doc-manager/src/propylon_document_manager/site/settings/base.py
+++ backend/doc-manager/src/propylon_document_manager/site/settings/base.py
@@ -2,6 +2,7 @@
 Base settings to build other settings files upon.
 """
 
+import os
 from pathlib import Path
 
 import environ
@@ -47,9 +48,13 @@ LOCALE_PATHS = [str(BASE_DIR / "locale")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "propylon_document_manager.sqlite",
-    }
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
+    },
 }
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
