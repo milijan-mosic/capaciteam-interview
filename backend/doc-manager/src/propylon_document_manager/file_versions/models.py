166163c1fb5b13a707e3cea3e2601fcc39c01d7e
milijan-mosic <milijan.mosic@gmail.com>
2025-09-12 14:01:24 +0200

    Created basic model for file

--- backend/doc-manager/src/propylon_document_manager/file_versions/models.py
+++ backend/doc-manager/src/propylon_document_manager/file_versions/models.py
@@ -1,6 +1,24 @@
+import uuid
+
 from django.db import models
 
 
 class FileVersion(models.Model):
-    file_name = models.fields.CharField(max_length=512)
-    version_number = models.fields.IntegerField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    #
+    name = models.CharField(max_length=255)
+    url = models.URLField()
+    version = models.PositiveIntegerField()
+    #
+    content = models.BinaryField()
+    #
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-version"]
+
+    def get_download_url(self):
+        from django.urls import reverse
+
+        return reverse("file_download", args=[str(self.id)])
